85dec93cb55a62b81330ef68d042688fa99d5c58
Kodpencerem <emrullah04@outlook.com>
2023-05-31 16:01:12 +0300

    Add web service client and configuring app.config

--- EmployeeWebServiceHost/Program.cs
+++ EmployeeWebServiceHost/Program.cs
@@ -1,9 +1,18 @@
-﻿namespace EmployeeWebServiceHost
+﻿using System;
+using System.ServiceModel;
+
+namespace EmployeeWebServiceHost
 {
     public class Program
     {
         static void Main(string[] args)
         {
+            using(ServiceHost host = new ServiceHost(typeof(EmployeeWebService.EmployeeService)))
+            {
+                host.Open();
+                Console.WriteLine("Host started @" + DateTime.Now);
+                Console.ReadLine();
+            }
         }
     }
 }
