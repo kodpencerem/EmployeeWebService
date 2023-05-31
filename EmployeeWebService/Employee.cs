8636efc7c9733378585d0b260a815618a3809224
Kodpencerem <emrullah04@outlook.com>
2023-05-31 16:50:13 +0300

    DataContract and DataMember

--- EmployeeWebService/Employee.cs
+++ EmployeeWebService/Employee.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace EmployeeWebService
 {
+    //[Serializable] tüm üyelerin isim sırasına göre gönderir. Esnekleklik yok. Tüm üyeler gönderilir. 
+    [DataContract] // Serializable aksine bir esneklik sağlar. DataMemeber ile hangi property'lerin gönderilecği işaretlenebilir. 
     public class Employee
     {
         private int _id;
@@ -9,24 +12,28 @@ namespace EmployeeWebService
         private string _gender;
         private DateTime _dateOfBirth;
 
+        [DataMember]
         public int Id
         {
             get { return _id; }
             set { _id = value; }
         }
 
+        [DataMember]
         public string Name
         {
             get { return _name; }
             set { _name = value; }
         }
 
+        [DataMember]
         public string Gender
         {
             get { return _gender; }
             set { _gender = value; }
         }
 
+        [DataMember]
         public DateTime DateOfBirth
         {
             get { return _dateOfBirth; }
